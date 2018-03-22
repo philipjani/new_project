83278434531a3a92cf06c705da2b7e5daee3701d
philipjani <philipjani@yahoo.com>
2018-03-22 05:26:57 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const express = require('express')
+const server = express();
+const hostname = 'localhost';
+const port = 3000;
+
+server.use(express.static('assets'));
+
+server.listen(port, () => {
+	console.log(`server running at ${hostname}:${port}`);
+});
